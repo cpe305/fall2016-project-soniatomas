83b2411966ecdc4853e663e30235e984a95939ef
Sonia Tomas <soniatomas723@gmail.com>
2016-12-09 11:26:00 -0800

    updating files

--- src/main/java/soniatomas/cpe305fall2016project/skinsort/DatabaseException.java
+++
@@ -1,8 +0,0 @@
-package main.java.soniatomas.cpe305fall2016project.skinsort;
-
-@SuppressWarnings("serial")
-public class DatabaseException extends Exception {
-  public DatabaseException() {
-    super("DatabaseException: Error connecting to database;");
-  }
-}
--- src/main/java/soniatomas/cpe305fall2016project/skinsort/View.java
+++
@@ -1,5 +0,0 @@
-package main.java.soniatomas.cpe305fall2016project.skinsort;
-
-public abstract class View {
-  public abstract void display();
-}
